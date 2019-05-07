e7415393cb550b6c115443f8f1b77068040d9c78
D7703_29 <d7703_29@d7703-29ui-imac.local>
2019-05-07 16:03:01 +0900

    Initial Commit

---
+++ simpleTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleTV
+//
+//  Created by D7703_29 on 2019. 5. 7..
+//  Copyright © 2019년 D7703_29. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
