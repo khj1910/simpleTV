ee6afd96fa4b916a64f5e27dd814da8afacad623
D7703_29 <d7703_29@d7703-29ui-imac.local>
2019-05-07 16:19:54 +0900

    commit

---
+++ simpleTV/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  simpleTV
+//
+//  Created by D7703_29 on 2019. 5. 7..
+//  Copyright © 2019년 D7703_29. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- simpleTV/ViewController.swift
+++ simpleTV/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var animals = ["cat","dog","pig","bird","whale"]
+    var detail = ["고양이","강아지","돼지","새","고래"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = animals[row]
+        cell.detailTextLabel?.text = detail[row]
+        
+        cell.imageView?.image = UIImage(named: animals[row])
+        
+        return cell
+    }
 }
+
+
 
